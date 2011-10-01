620867231650aaf9d5fd3ef90fdacdf2c319a0de
sedwards@willowgarage.com <sedwards@willowgarage.com@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2011-10-01 21:38:10 +0000

    Motoman package added to the repository.  Code includes motoplus applicatoins (run on a DX100 controller) and ros node applications(run on a PC) that enable ros remote control of a motoman robot connected to a DX100 type controller.  In order to use this package (as is), a motoman DX100 and motoplus license is required.  Specific controller configuration is also required.  Instructions for configuration are to be posted on the wiki (soon).
    A project level utilities directory is included.  Currently it only contains a google script for checking code style (not sure how useful this is going to be).

---
+++ industrial/motoman/motoplus/ros_socket.h
@@ -0,0 +1,58 @@
+﻿/*
+* Software License Agreement (BSD License) 
+*
+* Copyright (c) 2011, Yaskawa America, Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*       * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*       * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*       * Neither the name of the Yaskawa America, Inc., nor the names 
+*       of its contributors may be used to endorse or promote products derived
+*       from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/ 
+
+#ifndef __ros_socket_h
+#define __ros_socket_h
+
+#include "motoPlus.h"
+#include "definitions.h"
+#include "utils.h"
+
+class ROSSocket
+// Class for UDP socket object, used to communicate with ROS interface
+{
+	public:
+		ROSSocket(LONG port_num);
+		~ROSSocket(void);
+		LONG sendData(CHAR *raw_message, LONG data_length);
+		LONG sendMessage(LONG send_message[]);
+		LONG sendMessage(LONG p0, LONG p1, LONG p2, LONG p3, LONG p4, LONG p5, LONG p6, LONG p7, LONG p8, LONG p9, LONG p10);
+		LONG recvData(CHAR *raw_message);
+		LONG recvMessage(LONG recv_message[]);
+	
+	protected:
+		LONG sock_handle;
+		sockaddr_in server_sockaddr, client_sockaddr;
+		LONG rc, bytes_recv, bytes_send, sizeof_sockaddr;
+		ULONG port_num;
+};
+
+#endif
---
+++ industrial/motoman/motoplus/system.h
@@ -0,0 +1,74 @@
+﻿/*
+* Software License Agreement (BSD License) 
+*
+* Copyright (c) 2011, Yaskawa America, Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*       * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*       * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*       * Neither the name of the Yaskawa America, Inc., nor the names 
+*       of its contributors may be used to endorse or promote products derived
+*       from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/ 
+
+#ifndef __system_h
+#define __system_h
+
+#include "motoPlus.h"
+#include "definitions.h"
+#include "ros_socket.h"
+#include "utils.h"
+
+class System
+{
+  public:
+    System(ROSSocket* sock, bool* motion_allowed);
+    ~System(void);
+    LONG hold(bool hold_on);
+    void getFBPulse(LONG reply_message[]);
+    void getFBSpeed(LONG reply_message[]);
+    void getTorque(LONG reply_message[]);
+		
+  protected:		
+	// Hold variables
+	MP_HOLD_SEND_DATA hold_data;
+	MP_STD_RSP_DATA hold_error;
+		
+	// Pulse feedback variables
+	MP_CTRL_GRP_SEND_DATA pulse_send_data;
+	MP_FB_PULSE_POS_RSP_DATA pulse_recv_data;
+	
+	// Speed feedback variables
+	MP_CTRL_GRP_SEND_DATA speed_send_data;
+	MP_FB_SPEED_RSP_DATA speed_recv_data;
+	
+	// Torque variables
+	MP_CTRL_GRP_SEND_DATA torque_send_data;
+	MP_TORQUE_RSP_DATA torque_recv_data;
+	
+	//Socket
+	ROSSocket *sock;
+		
+	// Motion allowed flag
+	bool* motion_allowed;
+};
+
+#endif
