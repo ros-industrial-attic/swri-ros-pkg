dae343564eac37c657b842ce5019c76cde5bfc4f
shaun.edwards@gmail.com <shaun.edwards@gmail.com@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2012-01-08 00:38:21 +0000

    Rearranged shared enumerations between motoros_lib.h and ros_conversion.h.

--- industrial/motoman/motoplus/motoros_lib/ros_conversion.h
+++ industrial/motoman/motoplus/motoros_lib/ros_conversion.h
@@ -33,6 +33,7 @@
 #define ROS_CONVERSION_H
 
 #include "joint_data.h"
+#include "motoros_lib.h"
 #include "motoPlus.h"
 
 namespace motoman
@@ -84,20 +85,6 @@ typedef RosJointIndexes::RosJointIndex RosJointIndex;
 
 
 /**
- * \brief Enumeration of Motoman robot types (models).  Initially these are
- * being used to initialize joint rads->count conversion factors.
- */
-namespace MotomanRobotModels
-{
-enum MotomanRobotModel
-{
-  SIA_10D
-};
-}
-typedef MotomanRobotModels::MotomanRobotModel MotomanRobotModel;
-
-
-/**
  * \brief Initializes joint rads->enocder count conversion factors
  * This function must be manually called from within mpUsrRoot.
  * Failure to call this function results in conversion factors of
@@ -105,7 +92,7 @@ typedef MotomanRobotModels::MotomanRobotModel MotomanRobotModel;
  *
  * \param robot model (@see MotomanRobotModels)
  */
-void initJointConversion(MotomanRobotModel model_number);
+void initJointConversion(motoman::motoros_lib::MotomanRobotModel model_number);
 
 //TODO: Standardize function calls such that JointDatas are always in ROS
 //      order and MP types are always in motoman order.  The order of the
