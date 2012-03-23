2e846c99d084cf32bf18ede4b85ca4df4e001190
gjones@willowgarage.com <gjones@willowgarage.com@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2012-03-23 22:30:04 +0000

    A bunch of changes means that longhorn can move with MoveIt

--- branches/moveit_industrial/longhorn_arm_navigation/src/longhorn_manipulator_ikfast_plugin.cpp
+++ branches/moveit_industrial/longhorn_arm_navigation/src/longhorn_manipulator_ikfast_plugin.cpp
@@ -1,7 +1,8 @@
 #include <ros/ros.h>
 #include <kinematics_base/kinematics_base.h>
 #include <urdf/model.h>
-#include <arm_kinematics_constraint_aware/ik_fast_solver.h>
+#include <moveit_configuration_tools/ik_fast_solver.h>
+#include <planning_models/transforms.h>
 
 namespace longhorn_manipulator_kinematics
 {
@@ -16,14 +17,14 @@ class IKFastKinematicsPlugin : public kinematics::KinematicsBase
   std::vector<double> joint_max_vector_;
   std::vector<bool> joint_has_limits_vector_;
   std::vector<std::string> link_names_;
-  arm_kinematics_constraint_aware::ik_solver_base* ik_solver_;
+  moveit_configuration_tools::ik_solver_base* ik_solver_;
   size_t num_joints_;
   std::vector<int> free_params_;
   void (*fk)(const IKReal* j, IKReal* eetrans, IKReal* eerot);
   
 public:
 
-  IKFastKinematicsPlugin():ik_solver_(0) {}
+  IKFastKinematicsPlugin():ik_solver_(0), num_joints_(0) {}
   ~IKFastKinematicsPlugin(){ if(ik_solver_) delete ik_solver_;}
 
   void fillFreeParams(int count, int *array) { free_params_.clear(); for(int i=0; i<count;++i) free_params_.push_back(array[i]); }
@@ -31,7 +32,13 @@ public:
   bool initialize(const std::string& group_name,
                   const std::string& base_name,
                   const std::string& tip_name,
-                  const double& search_discretization) {
+                  double search_discretization) {
+
+    if(num_joints_ != 0) {
+      ROS_WARN_STREAM("Already initialized, not reinitializing");
+      return true;
+    }
+
     setValues(group_name, base_name, tip_name,search_discretization);
 
     ros::NodeHandle node_handle("~/"+group_name);
@@ -41,7 +48,7 @@ public:
       
     fillFreeParams(getNumFreeParameters(),getFreeParameters());
     num_joints_ = getNumJoints();
-    ik_solver_ = new arm_kinematics_constraint_aware::ikfast_solver<IKSolution>(ik, num_joints_);
+    ik_solver_ = new moveit_configuration_tools::ikfast_solver<IKSolution>(ik, num_joints_);
     fk=fk;
       
     if(free_params_.size()>1){
@@ -66,13 +73,14 @@ public:
     node_handle.param(full_urdf_xml,xml_string,std::string());
     robot_model.initString(xml_string);
 
-    boost::shared_ptr<urdf::Link> link = boost::const_pointer_cast<urdf::Link>(robot_model.getLink(tip_name_));
-    while(link->name != base_name_ && joint_names_.size() <= num_joints_){
+    boost::shared_ptr<urdf::Link> link = boost::const_pointer_cast<urdf::Link>(robot_model.getLink(tip_frame_));
+    while(link->name != base_frame_ && joint_names_.size() <= num_joints_){
       //	ROS_INFO("link %s",link->name.c_str());
       link_names_.push_back(link->name);
       boost::shared_ptr<urdf::Joint> joint = link->parent_joint;
       if(joint){
         if (joint->type != urdf::Joint::UNKNOWN && joint->type != urdf::Joint::FIXED) {
+          ROS_INFO_STREAM("Pushing back " << joint->name);
 	  joint_names_.push_back(joint->name);
           float lower, upper;
           int hasLimits;
@@ -107,7 +115,7 @@ public:
     }
     
     if(joint_names_.size() != num_joints_){
-      ROS_FATAL("Joints number mismatch.");
+      ROS_FATAL_STREAM("Joints number mismatch. Num joints " << num_joints_ << " joint names size is " << joint_names_.size());
       return false;
     }
       
@@ -125,7 +133,7 @@ public:
 
   bool getCount(int &count, 
                 const int &max_count, 
-                const int &min_count)
+                const int &min_count) const
   {
     if(count > 0)
     {
@@ -164,8 +172,8 @@ public:
   bool getPositionIK(const geometry_msgs::Pose &ik_pose,
                      const std::vector<double> &ik_seed_state,
                      std::vector<double> &solution,
-                     int &error_code) {
-	
+                     moveit_msgs::MoveItErrorCodes &error_code) const
+  {
     std::vector<double> vfree(free_params_.size());
     for(std::size_t i = 0; i < free_params_.size(); ++i){
       int p = free_params_[i];
@@ -173,11 +181,10 @@ public:
       vfree[i] = ik_seed_state[p];
     }
 
-    KDL::Frame frame;
-    tf::PoseMsgToKDL(ik_pose,frame);
+    Eigen::Affine3d frame;
+    planning_models::poseFromMsg(ik_pose, frame);
 
     int numsol = ik_solver_->solve(frame,vfree);
-
 		
     if(numsol){
       for(int s = 0; s < numsol; ++s){
@@ -193,27 +200,27 @@ public:
         }
         if(obeys_limits) {
           ik_solver_->getSolution(s,solution);
-          error_code = kinematics::SUCCESS;
+          error_code.val = error_code.SUCCESS;
           return true;
         }
       }
     }
 	
-    error_code = kinematics::NO_IK_SOLUTION; 
+    error_code.val = error_code.NO_IK_SOLUTION; 
     return false;
   }
   bool searchPositionIK(const geometry_msgs::Pose &ik_pose,
                         const std::vector<double> &ik_seed_state,
-                        const double &timeout,
+                        double timeout,
                         std::vector<double> &solution,
-                        int &error_code) {
-  
+                        moveit_msgs::MoveItErrorCodes &error_code) const
+  {
     if(free_params_.size()==0){
       return getPositionIK(ik_pose, ik_seed_state,solution, error_code);
     }
-	
-    KDL::Frame frame;
-    tf::PoseMsgToKDL(ik_pose,frame);
+
+    Eigen::Affine3d frame;
+    planning_models::poseFromMsg(ik_pose, frame);
 
     std::vector<double> vfree(free_params_.size());
 
@@ -249,7 +256,7 @@ public:
           }
           if(obeys_limits) {
             ik_solver_->getSolution(s,solution);
-            error_code = kinematics::SUCCESS;
+            error_code.val = error_code.SUCCESS;
             return true;
           }
         }
@@ -272,12 +279,12 @@ public:
       //     //ROS_INFO_STREAM("Num " << i << " value " << sol[i] << " has limits " << joint_has_limits_vector_[i] << " " << joint_min_vector_[i] << " " << joint_max_vector_[i]);
       //   }
       //   if(obeys_limits) {
-      //     error_code = kinematics::SUCCESS;
+      //     error_code.val = kinematics::SUCCESS;
       //     return true;
       //   }
       // }
       if(!getCount(counter, num_positive_increments, num_negative_increments)) {
-        error_code = kinematics::NO_IK_SOLUTION; 
+        error_code.val = error_code.NO_IK_SOLUTION; 
         return false;
       }
       
@@ -285,18 +292,18 @@ public:
       ROS_DEBUG_STREAM(counter << " " << vfree[0]);
     }
     //shouldn't ever get here
-    error_code = kinematics::NO_IK_SOLUTION; 
+    error_code.val = error_code.NO_IK_SOLUTION; 
     return false;
   }      
 
   bool searchPositionIK(const geometry_msgs::Pose &ik_pose,
                         const std::vector<double> &ik_seed_state,
-                        const double &timeout,
-                        const unsigned int& redundancy,
-                        const double &consistency_limit,
+                        double timeout,
+                        unsigned int redundancy,
+                        double consistency_limit,
                         std::vector<double> &solution,
-                        int &error_code) {
-
+                        moveit_msgs::MoveItErrorCodes &error_code) const
+  {
     if(free_params_.size()==0){
       //TODO - how to check consistency when there are no free params?
       return getPositionIK(ik_pose, ik_seed_state,solution, error_code);
@@ -307,9 +314,9 @@ public:
       ROS_WARN_STREAM("Calling consistency search with wrong free param");
       return false;
     }
-	
-    KDL::Frame frame;
-    tf::PoseMsgToKDL(ik_pose,frame);
+
+    Eigen::Affine3d frame;
+    planning_models::poseFromMsg(ik_pose, frame);
 
     std::vector<double> vfree(free_params_.size());
 
@@ -348,7 +355,7 @@ public:
           }
           if(obeys_limits) {
             ik_solver_->getSolution(s,solution);
-            error_code = kinematics::SUCCESS;
+            error_code.val = error_code.SUCCESS;
             return true;
           }
         }
@@ -371,12 +378,12 @@ public:
       //     //ROS_INFO_STREAM("Num " << i << " value " << sol[i] << " has limits " << joint_has_limits_vector_[i] << " " << joint_min_vector_[i] << " " << joint_max_vector_[i]);
       //   }
       //   if(obeys_limits) {
-      //     error_code = kinematics::SUCCESS;
+      //     error_code.val = kinematics::SUCCESS;
       //     return true;
       //   }
       // }
       if(!getCount(counter, num_positive_increments, num_negative_increments)) {
-        error_code = kinematics::NO_IK_SOLUTION; 
+        error_code.val = error_code.NO_IK_SOLUTION; 
         return false;
       }
       
@@ -384,26 +391,26 @@ public:
       ROS_DEBUG_STREAM(counter << " " << vfree[0]);
     }
     //shouldn't ever get here
-    error_code = kinematics::NO_IK_SOLUTION; 
+    error_code.val = error_code.NO_IK_SOLUTION; 
     return false;
   }      
 
   bool searchPositionIK(const geometry_msgs::Pose &ik_pose,
                         const std::vector<double> &ik_seed_state,
-                        const double &timeout,
+                        double timeout,
                         std::vector<double> &solution,
-                        const boost::function<void(const geometry_msgs::Pose &ik_pose,const std::vector<double> &ik_solution,int &error_code)> &desired_pose_callback,
-                        const boost::function<void(const geometry_msgs::Pose &ik_pose,const std::vector<double> &ik_solution,int &error_code)> &solution_callback,
-                        int &error_code){
-
+                        const kinematics::KinematicsBase::IKCallbackFn &desired_pose_callback,
+                        const kinematics::KinematicsBase::IKCallbackFn &solution_callback,
+                        moveit_msgs::MoveItErrorCodes &error_code) const
+  {
     if(free_params_.size()==0){
       if(!getPositionIK(ik_pose, ik_seed_state,solution, error_code)) {
         ROS_DEBUG_STREAM("No solution whatsoever");
-        error_code = kinematics::NO_IK_SOLUTION; 
+        error_code.val = error_code.NO_IK_SOLUTION; 
         return false;
       } 
       solution_callback(ik_pose,solution,error_code);
-      if(error_code == kinematics::SUCCESS) {
+      if(error_code.val == error_code.SUCCESS) {
         ROS_DEBUG_STREAM("Solution passes");
         return true;
       } else {
@@ -414,14 +421,14 @@ public:
 
     if(!desired_pose_callback.empty())
       desired_pose_callback(ik_pose,ik_seed_state,error_code);
-    if(error_code < 0)
+    if(error_code.val != error_code.SUCCESS)
     {
       ROS_ERROR("Could not find inverse kinematics for desired end-effector pose since the pose may be in collision");
       return false;
     }
 
-    KDL::Frame frame;
-    tf::PoseMsgToKDL(ik_pose,frame);
+    Eigen::Affine3d frame;
+    planning_models::poseFromMsg(ik_pose, frame);
 
     std::vector<double> vfree(free_params_.size());
 
@@ -455,7 +462,7 @@ public:
       if(numsol > 0){
         if(solution_callback.empty()){
           ik_solver_->getClosestSolution(ik_seed_state,solution);
-          error_code = kinematics::SUCCESS;
+          error_code.val = error_code.SUCCESS;
           return true;
         }
         
@@ -471,7 +478,7 @@ public:
           }
           if(obeys_limits) {
             solution_callback(ik_pose,sol,error_code);
-            if(error_code == kinematics::SUCCESS){
+            if(error_code.val == error_code.SUCCESS){
               solution = sol;
               ROS_DEBUG_STREAM("Took " << (ros::WallTime::now() - start) << " to return true " << countsol << " " << solvecount);
               return true;
@@ -480,35 +487,35 @@ public:
         }
       }
       if(!getCount(counter, num_positive_increments, num_negative_increments)) {
-        error_code = kinematics::NO_IK_SOLUTION; 
+        error_code.val = error_code.NO_IK_SOLUTION; 
         ROS_DEBUG_STREAM("Took " << (ros::WallTime::now() - start) << " to return false " << countsol << " " << solvecount);
         return false;
       }
       vfree[0] = initial_guess+search_discretization_*counter;
       ROS_DEBUG_STREAM(counter << " " << vfree[0]);
     }
-    error_code = kinematics::NO_IK_SOLUTION; 
+    error_code.val = error_code.NO_IK_SOLUTION; 
     return false;
   }      
 
   bool searchPositionIK(const geometry_msgs::Pose &ik_pose,
                         const std::vector<double> &ik_seed_state,
-                        const double &timeout,
-                        const unsigned int& redundancy,
-                        const double &consistency_limit,
+                        double timeout,
+                        unsigned int redundancy,
+                        double consistency_limit,
                         std::vector<double> &solution,
-                        const boost::function<void(const geometry_msgs::Pose &ik_pose,const std::vector<double> &ik_solution,int &error_code)> &desired_pose_callback,
-                        const boost::function<void(const geometry_msgs::Pose &ik_pose,const std::vector<double> &ik_solution,int &error_code)> &solution_callback,
-                        int &error_code){
-
+                        const kinematics::KinematicsBase::IKCallbackFn &desired_pose_callback,
+                        const kinematics::KinematicsBase::IKCallbackFn &solution_callback,
+                        moveit_msgs::MoveItErrorCodes &error_code) const
+  {
     if(free_params_.size()==0){
       if(!getPositionIK(ik_pose, ik_seed_state,solution, error_code)) {
         ROS_DEBUG_STREAM("No solution whatsoever");
-        error_code = kinematics::NO_IK_SOLUTION; 
+        error_code.val = error_code.NO_IK_SOLUTION; 
         return false;
       } 
       solution_callback(ik_pose,solution,error_code);
-      if(error_code == kinematics::SUCCESS) {
+      if(error_code.val == error_code.SUCCESS) {
         ROS_DEBUG_STREAM("Solution passes");
         return true;
       } else {
@@ -524,14 +531,14 @@ public:
 
     if(!desired_pose_callback.empty())
       desired_pose_callback(ik_pose,ik_seed_state,error_code);
-    if(error_code < 0)
+    if(error_code.val != error_code.SUCCESS)
     {
       ROS_ERROR("Could not find inverse kinematics for desired end-effector pose since the pose may be in collision");
       return false;
     }
 
-    KDL::Frame frame;
-    tf::PoseMsgToKDL(ik_pose,frame);
+    Eigen::Affine3d frame;
+    planning_models::poseFromMsg(ik_pose, frame);
 
     std::vector<double> vfree(free_params_.size());
 
@@ -568,7 +575,7 @@ public:
       if(numsol > 0){
         if(solution_callback.empty()){
           ik_solver_->getClosestSolution(ik_seed_state,solution);
-          error_code = kinematics::SUCCESS;
+          error_code.val = error_code.SUCCESS;
           return true;
         }
         
@@ -584,7 +591,7 @@ public:
           }
           if(obeys_limits) {
             solution_callback(ik_pose,sol,error_code);
-            if(error_code == kinematics::SUCCESS){
+            if(error_code.val == error_code.SUCCESS){
               solution = sol;
               ROS_DEBUG_STREAM("Took " << (ros::WallTime::now() - start) << " to return true " << countsol << " " << solvecount);
               return true;
@@ -593,44 +600,54 @@ public:
         }
       }
       if(!getCount(counter, num_positive_increments, num_negative_increments)) {
-        error_code = kinematics::NO_IK_SOLUTION; 
+        error_code.val = error_code.NO_IK_SOLUTION; 
         ROS_DEBUG_STREAM("Took " << (ros::WallTime::now() - start) << " to return false " << countsol << " " << solvecount);
         return false;
       }
       vfree[0] = initial_guess+search_discretization_*counter;
       ROS_DEBUG_STREAM(counter << " " << vfree[0]);
     }
-    error_code = kinematics::NO_IK_SOLUTION; 
+    error_code.val = error_code.NO_IK_SOLUTION; 
     return false;
   }      
 
   bool getPositionFK(const std::vector<std::string> &link_names,
                      const std::vector<double> &joint_angles, 
-                     std::vector<geometry_msgs::Pose> &poses){
-    return false;
-    
-    KDL::Frame p_out;
-
+                     std::vector<geometry_msgs::Pose> &poses) const
+  {
     if(link_names.size() == 0) {
       ROS_WARN_STREAM("Link names with nothing");
       return false;
     }
 
-    if(link_names.size()!=1 || link_names[0]!=tip_name_){
-      ROS_ERROR("Can compute FK for %s only",tip_name_.c_str());
+    if(link_names.size()!=1 || link_names[0]!=tip_frame_){
+      ROS_ERROR("Can compute FK for %s only",tip_frame_.c_str());
       return false;
     }
 	
     bool valid = true;
 	
-   double eerot[9], eetrans[3];
+    double eerot[9], eetrans[3];
     fk(&joint_angles[0],eetrans,eerot);
-    for(int i=0; i<3;++i) p_out.p.data[i] = eetrans[i];
-    for(int i=0; i<9;++i) p_out.M.data[i] = eerot[i];
-	
+    Eigen::Affine3d out_mat;
+    out_mat.translation().x() = eetrans[0];
+    out_mat.translation().y() = eetrans[1];
+    out_mat.translation().z() = eetrans[2];
+    out_mat(0,0) = eerot[0];
+    out_mat(0,1) = eerot[1];
+    out_mat(0,2) = eerot[2];
+
+    out_mat(1,0) = eerot[3];
+    out_mat(1,1) = eerot[4];
+    out_mat(1,2) = eerot[5];
+
+    out_mat(2,0) = eerot[6];
+    out_mat(2,1) = eerot[7];
+    out_mat(2,2) = eerot[8];
+
     poses.resize(1);
-    tf::PoseKDLToMsg(p_out,poses[0]);	
-	
+    planning_models::msgFromPose(out_mat, poses[0]);
+    
     return valid;
   }      
   const std::vector<std::string>& getJointNames() const { return joint_names_; }
