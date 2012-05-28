e299ee3147c828036e7e676390f453b909e0262e
shaun.edwards@gmail.com <shaun.edwards@gmail.com@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2012-05-28 21:57:34 +0000

    Deleting fuerte branch.  Branch wasn't created correctly to deal with externals.

--- branches/fuerte/swri_demos/armadillo_calibration/view_results/view_scatter.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-rosrun calibration_estimation error_visualization.py /tmp/armadillo_calibration/cal_measurements.bag /tmp/armadillo_calibration/ `rospack find armadillo_calibration`/view_results/scatter_config.yaml
