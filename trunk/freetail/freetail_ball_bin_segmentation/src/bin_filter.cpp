7c4cd7ddcf804e07e840ed96f3c5c4d315d94627
gomez.christina.x@gmail.com <gomez.christina.x@gmail.com@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2012-08-23 23:04:12 +0000

    Updated source code to include RGB to HSV/I conversions in order to delete the red bin points from the cluster. Additionally added pcl ransac sphere fitting code (not exactly properly functional).

--- trunk/freetail/freetail_ball_bin_segmentation/src/bin_filter.cpp
+++ trunk/freetail/freetail_ball_bin_segmentation/src/bin_filter.cpp
@@ -1,7 +1,6 @@
 #include "ros/ros.h"
 #include <algorithm>
-using std::max;
-using std::min;
+using namespace std;
 
 #include "std_msgs/String.h"
 #include <sensor_msgs/PointCloud.h>
@@ -9,6 +8,7 @@ using std::min;
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/point_types_conversion.h>
 #include <boost/foreach.hpp>
 #include "ros/console.h"
 
@@ -24,8 +24,12 @@ using std::min;
 #include <pcl/kdtree/kdtree.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_plane.h>
+#include <pcl/sample_consensus/sac_model_sphere.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
+#include <pcl/point_cloud.h>
 
 #include "/opt/ros/fuerte/stacks/pr2_object_manipulation/perception/tabletop_object_detector/srv_gen/cpp/include/tabletop_object_detector/TabletopSegmentation.h"
 #include </opt/ros/fuerte/stacks/pr2_object_manipulation/perception/tabletop_object_detector/srv_gen/cpp/include/tabletop_object_detector/TabletopObjectRecognition.h>
@@ -34,6 +38,10 @@ using std::min;
  */
 
 ros::Publisher pub;
+ros::Publisher pub2;
+ros::Publisher pub3;
+ros::Publisher pubt;
+ros::Publisher pubs;
 ros::ServiceClient seg_srv;
 ros::ServiceClient rec_srv;
 
@@ -65,11 +73,178 @@ void pointcloudcallback(const sensor_msgs::PointCloud2ConstPtr& pcmsg)
   sensor_msgs::convertPointCloudToPointCloud2(clustervector, clusters);
   clusters.header.frame_id=segmentation_srv.response.table.pose.header.frame_id;
   clusters.header.stamp=segmentation_srv.response.table.pose.header.stamp;
+  pub2.publish (clusters);
+
+//definitely not the table...
+/*  sensor_msgs::PointCloud table;
+  sensor_msgs::PointCloud2 tablecluster;
+  int end = segmentation_srv.response.clusters.size()-1;
+  table=segmentation_srv.response.clusters[end];
+  sensor_msgs::convertPointCloudToPointCloud2(table, tablecluster);
+  tablecluster.header.frame_id=segmentation_srv.response.table.pose.header.frame_id;
+  tablecluster.header.stamp=segmentation_srv.response.table.pose.header.stamp;
+  pubt.publish(tablecluster);
+*/
+  //TAKE CLUSTER OF BIN/BALL AND REMOVE RED (BIN)
+  pcl::PointCloud<pcl::PointXYZRGB> PCxyzrgb;// (new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::fromROSMsg(clusters,PCxyzrgb);
+
+  pcl::PointCloud<pcl::PointXYZI> PCxyzi;
+  pcl::PointCloudXYZRGBtoXYZI(PCxyzrgb, PCxyzi);
+  sensor_msgs::PointCloud2 clusterint;
+
+  pcl::toROSMsg(PCxyzi, clusterint);
+  clusterint.header.frame_id="/base_link";
+  clusterint.header.stamp=ros::Time::now();
+   //publish clusters for rviz
+   pub3.publish (clusterint);
+
+  ROS_INFO("Cluster of bin with balls - %d points", (int)PCxyzrgb.width);
+  //ROS_INFO("Cluster of bin with balls - height %d", (int)PCxyzrgb.height);
+  //width - the total number of points in the cloud (equal with POINTS see below) for unorganized datasets
+  int pcsize=PCxyzrgb.width;
+  std::vector<int> ivec;
+  for (int count=0; count < pcsize; count++)
+  {
+	  float r1, g1, b1;
+	  r1=PCxyzrgb.at(count).r;
+	  g1=PCxyzrgb.at(count).g;
+	  b1=PCxyzrgb.at(count).b;
+/*
+
+	  r1=255; g1=0; b1=0;
+*/	  //convert RGB to HSV
+	  float r, g, b;
+	  r=r1/255.0; g=g1/255.0; b=b1/255.0;
+	  float M, m, C;
+	  M=max(max(r,g),b);
+	  m=min(min(r,g),b);
+	  C=M-m;
+/*
+	  ROS_INFO("Current chroma value: %4.2f", (float)C);
+	  ROS_INFO("Current r value: %4.2f", (float)r);
+	  ROS_INFO("Current g value: %4.2f", (float)g);
+	  ROS_INFO("Current b value: %4.2f", (float)b);
+	  //ROS_INFO("Current max value: %4.2f", (float)M);
+*/
+	  float H1, H, V, S;
+	  if (C == 0.0)
+		{
+		  H=0;
+		}
+		else if (M==r)
+		{
+		  H=60*((g-b)/C);
+		}
+		else if (M==g)
+		{
+		  H=120+60*((b-r)/C);
+		}
+		else if (M==b)
+		{
+		  H=240+60*((r-g)/C);
+		}
+		else
+			H=0;
+
+	  if (H<0)
+	  {
+		  H+=360;
+	  }
+	  V=M;
+
+	  if (V==0.0)
+	  {
+		  S=0.0;
+	  }
+	  else
+	  {
+		  S=C/V;
+	  }
+	  float I=(r+g+b)/3.0;
+	  float S3=1-(m/I);
+	  double alpha = 0.5*(2*r-g-b);
+	  double beta = (sqrt(3)/2.0)*(g-b);
+	  double H2 = atan2(beta, alpha);
+/*
+	  ROS_INFO("Current H value: %4.2f", (float)H);
+	  ROS_INFO("Current V value: %4.2f", (float)V);
+	  ROS_INFO("Current S value: %4.2f", (float)S);
+
+	  double alpha = 0.5*(2*r-g-b);
+	  double beta = (sqrt(3)/2)*(g-b);
+	  double H2 = atan2(beta, alpha);
+	  double C2 = sqrt((alpha*alpha)+(beta*beta));
+	  float S2, V2;
+	  V2=M;
+	  if (C2==0.0)
+	  	  {
+	  		  S2=0.0;
+	  	  }
+	  	  else
+	  	  {
+	  		  S2=C2/V2;
+	  	  }
+
+	  float CHr=r1/(r1+g1+b1);
+	  float CHg=g1/(r1+g1+b1);
+*/
+	  //if H~0 and S~1 and v~1, then the points should be red
+	  //if (H<=90 &&  V>=0.8)//S>=0.6 &&
+	  //if (H2<=30 && S2>=0.6 && V>=0.2)
+	  //if (r>0.5 && g<0.15 && b<0.15)
+	  //if (r1>=100 && g1<=50 && b1<=50)
+	  //if (CHr>=0.5 && CHr <= 1.0 && CHg<=0.15)
+	  if (H2<=1 && H2>=-1)//&& S3>0.95)//  && H2>=-0.15)// && S3<0.1)H2<=0.01
+	  {
+		  ivec.push_back(count);
+
+		  /*ROS_INFO("Current H2 value: %4.2f", (float)H2);
+		  ROS_INFO("Current I value: %4.2f", (float)I);
+		  ROS_INFO("Current S3 value: %4.2f", (float)S3);*/
+	  }
+
+  }
+  for (int i=0; i < ivec.size(); i++)
+    {
+	  PCxyzrgb.erase(PCxyzrgb.begin()+ivec[i]);
+    }
+
+  //ROS_INFO("random point in vector at position 40 %d", (double)alpha);
+  ROS_INFO("Cluster of balls - width %d", (int)PCxyzrgb.width);
+
+  sensor_msgs::PointCloud2 ballcluster;
+  pcl::toROSMsg(PCxyzrgb, ballcluster);
 
   //publish clusters for rviz
-  pub.publish (clusters);
+  pub.publish (ballcluster);
 
-  //TAKE CLUSTER OF BIN/BALL AND REMOVE RED (BIN)
+
+  //FIT SPHERES
+  pcl::PointCloud<pcl::PointXYZ>::Ptr PCxyz (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromROSMsg(ballcluster,*PCxyz);
+  //pcl::PointCloud<pcl::PointXYZ> PCxyz;
+  //pcl::PointCloudXYZRGBtoXYZ(PCxyzrgb, PCxyz);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr final (new pcl::PointCloud<pcl::PointXYZ>);
+  //pcl::PointCloud<pcl::PointXYZ> final;
+  std::vector<int> inliers;
+  pcl::SampleConsensusModelSphere<pcl::PointXYZ>::Ptr
+      model_s(new pcl::SampleConsensusModelSphere<pcl::PointXYZ> (PCxyz));
+
+  pcl::RandomSampleConsensus<pcl::PointXYZ> ransac (model_s);
+  ransac.setDistanceThreshold (.005);
+  ransac.computeModel();
+  ransac.getInliers(inliers);
+
+  // copies all inliers of the model computed to another PointCloud
+  pcl::copyPointCloud<pcl::PointXYZ>(*PCxyz, inliers, *final);
+
+
+  sensor_msgs::PointCloud2 spheres;
+  pcl::toROSMsg(*final,spheres);
+  //publish clusters for rviz
+  pubs.publish (spheres);
+
 
   /* SEGMENTATION USING ONLY PCL ALGORITHMS
 
@@ -206,7 +381,11 @@ int main(int argc, char **argv)
 */
 
   //publish topic for rviz visualization
-  pub = n.advertise<sensor_msgs::PointCloud2>("clusters", 100);
+  pub = n.advertise<sensor_msgs::PointCloud2>("ball_cluster", 100);
+  pub2 = n.advertise<sensor_msgs::PointCloud2>("bin_cluster", 100);
+  pub3 = n.advertise<sensor_msgs::PointCloud2>("cluster_intensity", 100);
+  pubt = n.advertise<sensor_msgs::PointCloud2>("table", 100);
+  pubs = n.advertise<sensor_msgs::PointCloud2>("spheres", 100);
 
   ros::spin();
 
