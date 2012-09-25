240825d2695af91d191c522de9df192c5566a16e
shaun.edwards@gmail.com <shaun.edwards@gmail.com@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2012-09-25 17:40:58 +0000

    Created suction_gripper node that interfaces with EtherCAT IO and exposes a grasp execution action interface.  Low level integration testing performed.

--- freetail/freetail_io/src/SuctionGripperGraspExecutionAction.cpp
+++ freetail/freetail_io/src/SuctionGripperGraspExecutionAction.cpp
@@ -33,16 +33,25 @@
 #include <ros/ros.h>
 #include <actionlib/server/action_server.h>
 #include <object_manipulation_msgs/GraspHandPostureExecutionAction.h>
-#include "object_manipulation_msgs/GraspHandPostureExecutionGoal.h"
+#include <object_manipulation_msgs/GraspHandPostureExecutionGoal.h>
+#include <soem_beckhoff_drivers/DigitalMsg.h>
 
 using namespace object_manipulation_msgs;
 using namespace actionlib;
+
+enum DigitalOutputType
+{
+ COLLISION = 0,
+ SUCTION = 1,
+ COUNT = 8
+};
 
 class SuctionGripperGraspExecutionAction
 {
 private:
   typedef ActionServer<GraspHandPostureExecutionAction> GEAS;
   typedef GEAS::GoalHandle GoalHandle;
+
 public:
   SuctionGripperGraspExecutionAction(ros::NodeHandle &n) :
     node_(n),
@@ -54,6 +63,18 @@ public:
     ros::NodeHandle pn("~");
     action_server_.start();
     ROS_INFO_STREAM("Grasp execution action node started");
+	pub_ = node_.advertise<soem_beckhoff_drivers::DigitalMsg>("digital_outputs", 1);
+
+	while ( pub_.getNumSubscribers() <= 0 && ros::ok())
+	{
+		ros::Duration(5.0).sleep();
+	    ROS_INFO_STREAM("Waiting for digital output subscribers");
+	}
+
+	msg_.values.resize(COUNT, false);
+	msg_.values[COLLISION] = true;
+	pub_.publish(msg_);
+    ROS_INFO_STREAM("Turning on air pressure to collision sensor");
   }
 
   ~SuctionGripperGraspExecutionAction()
@@ -76,14 +97,16 @@ private:
 			//gh.getGoal()->grasp
 			ROS_INFO("%s",(nodeName + ": Pre-grasp command accepted").c_str());
 			gh.setSucceeded();
+			msg_.values[SUCTION] = true;
+			pub_.publish(msg_);
 			break;
 
 		case GraspHandPostureExecutionGoal::GRASP:
 			gh.setAccepted();
 			ROS_INFO("%s",(nodeName + ": Executing a gripper grasp").c_str());
-
-			// wait
-			ros::Duration(1.0f).sleep();
+			msg_.values[SUCTION] = true;
+			pub_.publish(msg_);
+			ros::Duration(0.5f).sleep();
 			gh.setSucceeded();
 			break;
 
@@ -91,16 +114,16 @@ private:
 			gh.setAccepted();
 			ROS_INFO("%s",(nodeName + ": Executing a gripper release").c_str());
 
-			// wait
-			ros::Duration(1.0f).sleep();
+			msg_.values[SUCTION] = false;
+			pub_.publish(msg_);
+			ros::Duration(0.5f).sleep();
 			gh.setSucceeded();
 			break;
 
 		default:
 
 			ROS_INFO("%s",(nodeName + ": Unidentified grasp request, rejecting goal").c_str());
-			gh.setSucceeded();
-			//gh.setRejected();
+			gh.setRejected();
 			break;
 	}
 
@@ -110,14 +133,14 @@ private:
   {
     std::string nodeName = ros::this_node::getName();
 	ROS_INFO("%s",(nodeName + ": Canceling current grasp action").c_str());
-    //gh.setAccepted();
     gh.setCanceled();
     ROS_INFO("%s",(nodeName + ": Current grasp action has been canceled").c_str());
   }
 
   ros::NodeHandle node_;
   GEAS action_server_;
-  ros::publisher pub_;
+  ros::Publisher pub_;
+  soem_beckhoff_drivers::DigitalMsg msg_;
 
 };
 
@@ -125,7 +148,7 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "grasp_execution_action_node");
 	ros::NodeHandle node;
-	GraspExecutionAction ge(node);
+	SuctionGripperGraspExecutionAction ge(node);
 	ros::spin();
   return 0;
 }
