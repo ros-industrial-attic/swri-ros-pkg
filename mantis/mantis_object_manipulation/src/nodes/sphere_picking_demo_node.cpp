e5e0670acb1a94983dbcf3fc69256ff597b4bfe4
jrgnicho@gmail.com <jrgnicho@gmail.com@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2012-10-19 23:11:55 +0000

    SpherePickingRobotNavigator implements a subclass of RobotNavigator for sphere picking

--- mantis/mantis_object_manipulation/src/nodes/sphere_picking_demo_node.cpp
+++ mantis/mantis_object_manipulation/src/nodes/sphere_picking_demo_node.cpp
@@ -1,10 +1,9 @@
 /*
- * arm_pick_place_demo.cpp
+ * sphere_picking_demo_node.cpp
  *
- *  Created on: Oct 10, 2012
- *      Author: jnicho
+ *  Created on: Oct 19, 2012
  */
-#include <mantis_object_manipulation/arm_navigators/RobotPickPlaceNavigator.h>
+#include <mantis_object_manipulation/arm_navigators/SpherePickingRobotNavigator.h>
 
 int main(int argc,char** argv)
 {
@@ -12,9 +11,9 @@ int main(int argc,char** argv)
 	ros::NodeHandle nh;
 
 	ROS_INFO_STREAM(ros::this_node::getName()<<": Creating navigator");
-	RobotPickPlaceNavigator navigator(RobotPickPlaceNavigator::SETUP_SPHERE_PICK_PLACE);
+	RobotNavigator::Ptr navigator(new SpherePickingRobotNavigator());
 
 	ROS_INFO_STREAM(ros::this_node::getName()<<": Navigator started");
-	navigator.run();
+	navigator->run();
 	return 0;
 }
