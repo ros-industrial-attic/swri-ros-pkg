73e80b2905e49987ba6a436c22748b50c318a724
aviator79 <aviator79@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2012-10-31 14:29:36 +0000

    initial commit of nrg_object_recognition and data_collection

--- freetail/data_collection/src/euclidean_segmentation.h
+++ freetail/data_collection/src/euclidean_segmentation.h
@@ -16,13 +16,14 @@ SegmentCloud(sensor_msgs::PointCloud2 rawCloud, std::vector<pcl::PointCloud<pcl:
 {
   // Read in the cloud data
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+  std::cout << "In SegmentCloud, reading PointCloud2 msg" << std::endl;
   pcl::fromROSMsg(rawCloud, *cloud);
   
   // Create the filtering object: downsample the dataset using a leaf size of 1cm
   pcl::VoxelGrid<pcl::PointXYZ> vg;
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>), cloud_filtered_0 (new pcl::PointCloud<pcl::PointXYZ>);
   vg.setInputCloud (cloud);
-  vg.setLeafSize (0.01f, 0.01f, 0.01f);
+  vg.setLeafSize (0.001f, 0.001f, 0.001f);
   vg.filter (*cloud_filtered_0);
 
   // Create the segmentation object for the planar model and set all the parameters
@@ -36,25 +37,21 @@ SegmentCloud(sensor_msgs::PointCloud2 rawCloud, std::vector<pcl::PointCloud<pcl:
   seg.setMethodType (pcl::SAC_RANSAC);
   seg.setMaxIterations (100);
   seg.setDistanceThreshold (0.02);
-
   
-  //Spatial filter.
+    //Spatial filter.
   cloud_filtered->resize(0);
   //Parameters
-  float min_x = -0.5, max_x = 0.5;
-  float min_y = -2.5, max_y = 2.5;
-  float min_z = 0, max_z = 1.5;
-  
-//   float min_x = -0.5, max_x = 0.5;
-//   float min_y = -.5, max_y = .5;
-//   float min_z = 0, max_z = 1.5;
-  
-  for(pcl::PointCloud<pcl::PointXYZ>::iterator position=cloud_filtered_0->begin(); position!=cloud_filtered_0->end(); position++){
+ float min_x = -.2, max_x = .2;
+ float min_y = -.5, max_y = .5;
+ float min_z = .55, max_z = 1.15;
+ 
+ for(pcl::PointCloud<pcl::PointXYZ>::iterator position=cloud_filtered_0->begin(); position!=cloud_filtered_0->end(); position++){
     if(position->x > min_x && position->x < max_x && position->y > min_y && position->y < max_y && position->z > min_z && position->z < max_z)
       cloud_filtered->push_back(*position);
   }
-  int nr_points = (int) cloud_filtered->points.size ();
-  
+
+  std::cout << "num points in spatially filtered cloud: " << cloud_filtered->points.size() << std::endl;
+  int i=0, nr_points = (int) cloud_filtered->points.size ();
   while (cloud_filtered->points.size () > 0.3 * nr_points)
   {
     // Segment the largest planar component from the remaining cloud
@@ -66,6 +63,7 @@ SegmentCloud(sensor_msgs::PointCloud2 rawCloud, std::vector<pcl::PointCloud<pcl:
       break;
     }
 
+    std::cout << "extracting planar inliers..."  << std::endl;
     // Extract the planar inliers from the input cloud
     pcl::ExtractIndices<pcl::PointXYZ> extract;
     extract.setInputCloud (cloud_filtered);
@@ -74,14 +72,18 @@ SegmentCloud(sensor_msgs::PointCloud2 rawCloud, std::vector<pcl::PointCloud<pcl:
 
     // Write the planar inliers to disk
     extract.filter (*cloud_plane);
-    //std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
+    std::cout << "done."  << std::endl;
+    std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
 
+    std::cout << "Removing tabletop..."  << std::endl;
     // Remove the planar inliers, extract the rest
     extract.setNegative (true);
     extract.filter (*cloud_f);
     cloud_filtered = cloud_f;
+    std::cout << "done."  << std::endl;
   }
 
+     std::cout << "Number of points in remaining clusters: " << cloud_filtered->points.size()  << std::endl;
   // Creating the KdTree object for the search method of the extraction
   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
   tree->setInputCloud (cloud_filtered);
@@ -89,8 +91,8 @@ SegmentCloud(sensor_msgs::PointCloud2 rawCloud, std::vector<pcl::PointCloud<pcl:
   std::vector<pcl::PointIndices> cluster_indices;
   pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
   ec.setClusterTolerance (0.02); // 2cm
-  ec.setMinClusterSize (150);
-  ec.setMaxClusterSize (700);
+  ec.setMinClusterSize (0);
+  ec.setMaxClusterSize (25000);
   ec.setSearchMethod (tree);
   ec.setInputCloud (cloud_filtered);
   ec.extract (cluster_indices);
