9cde8465d5ac5792ef1d4194728883242107a50f
jrgnicho@gmail.com <jrgnicho@gmail.com@076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd>
2013-01-04 00:01:50 +0000

    Tag of mantis demo stable version as of 01032013

---
+++ tags/mantis_demo_01032013/freetail/freetail_object_manipulation/src/demos/BallPickingDemoNode.cpp
@@ -0,0 +1,20 @@
+/*
+ * BallPickingDemoNode.cpp
+ *
+ *  Created on: Sep 21, 2012
+ *      Author: jnicho
+ */
+
+#include <freetail_object_manipulation/demos/SimpleManipulationDemo.h>
+
+int main(int argc,char** argv)
+{
+	ros::init(argc,argv,"ball_picking_demo",ros::init_options::NoSigintHandler);
+
+	SimpleManipulationDemo demo;
+	demo.runBallPickingDemo();
+
+	ros::shutdown();
+
+	return 0;
+}
